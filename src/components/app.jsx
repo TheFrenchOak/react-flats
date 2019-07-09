50e6b07b179b8445285aace43a25fddd2b6bc9ed
Frederic Laffont <frederic_laffont@msn.com>
2019-07-09 20:52:16 +0200

    complete react-map exercice

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,17 +1,42 @@
 import React, { Component } from 'react';
-import Map from "./map";
-import FlatList from "./flat-list";
-import flats from "../../data/flats";
+import GoogleMapReact from 'google-map-react';
+
+import flats from '../../data/flats';
+import FlatList from './flat_list';
+import Marker from './marker';
 
 class App extends Component {
-  render () {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFlat: flats[0],
+      flats
+    };
+  }
+  
+  selectFlat = (index) => {
+    this.setState({ selectedFlat: flats[index] });
+  }
+
+  center() {
+    return {
+      lat: this.state.selectedFlat.lat,
+      lng: this.state.selectedFlat.lng
+    };
+  }
+
+  render() {
     return (
       <div>
-        <div className="flat-list">
-          <FlatList flats={flats} />
-        </div>
+        <FlatList
+          flats={this.state.flats}
+          selectedFlat={this.state.selectedFlat}
+          selectFlat={this.selectFlat}
+        />
         <div className="map-container">
-          <Map />
+          <GoogleMapReact center={this.center()} defaultZoom={12} bootstrapURLKeys={{ key: 'AIzaSyB_RT5j23hWFaxweruTai_jboHpdph0Yjc' }}>
+            <Marker lat={this.state.selectedFlat.lat} lng={this.state.selectedFlat.lng} />
+          </GoogleMapReact>
         </div>
       </div>
     );
